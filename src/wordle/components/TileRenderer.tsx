4e7fb606891cc27f19819ef28354502ddadc1766
Kevin Wentland <kwentland@threekit.com>
2022-04-07 17:26:48 -0500

    chore: implement real wordle logic

--- src/wordle/components/TileRenderer.tsx
+++ src/wordle/components/TileRenderer.tsx
@@ -13,22 +13,31 @@ const getTileRow = (
   locked: boolean,
   row: number
 ) => {
-  const getColor = (index: number) => {
-    if (locked) {
-      if (guess.length === 0) return 'grey';
-      else if (guess[index] === answer[index]) return 'green';
-      else if (answer.includes(guess[index])) return 'yellow';
-      else return 'red';
-    } else {
-      return 'white';
+  const colorArray = new Array(answer.length).fill(locked ? 'grey' : 'white');
+  const mutableAnswer = [...answer];
+
+  if (guess.length > 0 && locked) {
+    for (let pass = 0; pass < 3; pass++) {
+      guess.forEach((letter, index) => {
+        if (colorArray[index] !== 'grey') {
+          // continue...
+        } else if (pass === 0 && mutableAnswer[index] === letter) {
+          mutableAnswer[index] = '_';
+          colorArray[index] = 'green';
+        } else if (pass === 1 && mutableAnswer.includes(letter)) {
+          mutableAnswer[mutableAnswer.indexOf(letter)] = '_';
+          colorArray[index] = 'yellow';
+        } else if (pass === 2) {
+          colorArray[index] = 'red';
+        }
+      });
     }
-  };
+  }
   return (
     <div className={styles.tileRow} key={row}>
       {answer.map((char, i) => {
-        const color = getColor(i);
         const letter = guess[i] ?? '';
-        return <Tile text={letter} color={color} key={i} />;
+        return <Tile text={letter} color={colorArray[i]} key={i} />;
       })}
     </div>
   );
