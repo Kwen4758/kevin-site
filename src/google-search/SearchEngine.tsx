d2a793f0e042bcae6bd9243d9e984eac186293d8
Kevin Wentland <kwentland@threekit.com>
2022-04-08 13:45:06 -0500

    chore: add home link

--- src/google-search/SearchEngine.tsx
+++ src/google-search/SearchEngine.tsx
@@ -1,8 +1,12 @@
+import { Link } from 'react-router-dom';
 import styles from '../app/app.module.css';
 import MainPage from './components/MainPage';
 
 const SearchEngine = () => {
   return <div className={styles.app}>
+    <Link to="/" className={styles.appLink}>
+        HOME
+      </Link>
     <MainPage />
   </div>;
 };
