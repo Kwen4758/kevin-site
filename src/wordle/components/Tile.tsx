85ae610d6ea8e6b1660ffde32fc6cfc49121f59c
Kevin Wentland <kwentland@threekit.com>
2022-04-09 15:57:46 -0500

    chore: better wordle colors

--- src/wordle/components/Tile.tsx
+++ src/wordle/components/Tile.tsx
@@ -3,11 +3,12 @@ import styles from './wordle.module.css';
 interface TileProps {
   text: string;
   color: string;
+  textColor: string,
 }
 
-const Tile = ({ text, color }: TileProps) => {
+const Tile = ({ text, color, textColor }: TileProps) => {
   return (
-    <div className={styles.tile} style={{ backgroundColor: color }}>
+    <div className={styles.tile} style={{ backgroundColor: color, color: textColor }}>
       {text}
     </div>
   );
--- src/wordle/components/TileRenderer.tsx
+++ src/wordle/components/TileRenderer.tsx
@@ -13,22 +13,22 @@ const getTileRow = (
   locked: boolean,
   row: number
 ) => {
-  const colorArray = new Array(answer.length).fill(locked ? 'grey' : 'white');
+  const colorArray = new Array(answer.length).fill(locked ? 'black' : 'white');
   const mutableAnswer = [...answer];
 
   if (guess.length > 0 && locked) {
     for (let pass = 0; pass < 3; pass++) {
       guess.forEach((letter, index) => {
-        if (colorArray[index] !== 'grey') {
+        if (colorArray[index] !== 'black') {
           // continue...
         } else if (pass === 0 && mutableAnswer[index] === letter) {
           mutableAnswer[index] = '_';
-          colorArray[index] = 'green';
+          colorArray[index] = '#538d4e';
         } else if (pass === 1 && mutableAnswer.includes(letter)) {
           mutableAnswer[mutableAnswer.indexOf(letter)] = '_';
-          colorArray[index] = 'yellow';
+          colorArray[index] = '#b59f3b';
         } else if (pass === 2) {
-          colorArray[index] = 'red';
+          colorArray[index] = '#3a3a3c';
         }
       });
     }
@@ -37,7 +37,14 @@ const getTileRow = (
     <div className={styles.tileRow} key={row}>
       {answer.map((char, i) => {
         const letter = guess[i] ?? '';
-        return <Tile text={letter} color={colorArray[i]} key={i} />;
+        return (
+          <Tile
+            text={letter}
+            color={colorArray[i]}
+            textColor={locked ? 'white' : 'black'}
+            key={i}
+          />
+        );
       })}
     </div>
   );
